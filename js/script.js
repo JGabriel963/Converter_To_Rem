e6f37ce1e065c33c1e2a0f6094be5d1a8a7ef9e5
João Gabriel <joaogabriel9633@gmail.com>
2023-04-10 10:52:11 -0300

    Add event keydown e copyToRem

--- js/script.js
+++ js/script.js
@@ -1,7 +1,33 @@
 const res = document.querySelector('.res')
+const input = document.getElementById('ipx')
+const btnCovert = document.getElementById('btnConvert')
 
-document.getElementById('btnConvert').addEventListener('click', () => {
-    const pixel = document.getElementById('ipx').value;
-    const rem = pixel * 0.0625
+async function copyToRem() {
+    if (btnCovert.textContent === 'Copy') {
+        btnCovert.textContent = 'Copied'
+        try {
+            await navigator.clipboard.writeText(res.textContent);
+        } catch {
+            console.log('Erro ao copiar');
+        }
+    }
+}
+
+function toConvert() {
+    const rem = input.value * 0.0625
     res.textContent = `${rem}rem`
+    btnCovert.textContent = 'Copy'
+}
+
+input.addEventListener('keydown', function (ev) {
+    if (ev.key === "Enter") {
+        toConvert();
+    }
 })
+
+input.addEventListener('focus', () => {
+    btnCovert.textContent = "CONVERTER"
+})
+
+btnCovert.addEventListener('click', copyToRem)
+document.getElementById('btnConvert').addEventListener('click', toConvert)
